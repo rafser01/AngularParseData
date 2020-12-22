f95b493ffdbccee28a5b5a88118236c43c260846
Rakin Afser <r.afser01@gmail.com>
2020-12-22 15:45:14 +0600

    modified design; added card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 import { ModelModule } from './model/model.module';
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ModelModule } from './model/model.module';
     BrowserAnimationsModule,
     MatGridListModule,
     FormsModule,
-    ModelModule
+    ModelModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
